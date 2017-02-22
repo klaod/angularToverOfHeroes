c243b15489b1c51f382a4ab70623cfa17248fa04
klaod <you@example.com>
2017-02-23 00:39:55 +0200

    addded heroesList and heroes details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+    heroes: Hero[] = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+   }
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
