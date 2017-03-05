fa098c1b849802c9500602a97eeeb9db6a9fe8de
klaod <you@example.com>
2017-03-05 22:47:10 +0200

    added heroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
+import { HeroService } from './hero.service';
 import { Hero } from './objects/hero';
 import { Component } from '@angular/core';
- 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [HeroService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
-    selectedHero: Hero;
-    heroes: Hero[];
+   title = 'Tour of Heroes';
+   selectedHero: Hero;
+   heroes: Hero[];
+   constructor(private heroService: HeroService) { }
 
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
+   ngOnInit(): void {
+     this.getHeroesSlowly();
+   }
+
+   onSelect(hero: Hero): void {
+     this.selectedHero = hero;
+   }
+   getHeroesSlowly(): void {
+     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
    }
 }
 
---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Hero } from './objects/hero';
+import { HEROES } from './objects/moke-heroes';
+import { Injectable } from '@angular/core';
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+  
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 1000);
+    });
+  }
+}
