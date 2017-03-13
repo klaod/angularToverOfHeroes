89cd1e84c72befd9230e1ebcf665461f71f6478e
klaod <reznichenkovo@gmail.com>
2017-03-13 15:04:40 +0200

    добавлен сервис поиска

--- src/app/services/hero-search.service.ts
+++ src/app/services/hero-search.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http }       from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { Hero }           from '../objects/hero';
+
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class HeroSearchService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  search(term: string): Observable<Hero[]> {
+    return this.http
+               .get(`app/heroes/?name=${term}`)
+               .map(response => response.json().data as Hero[]);
+  }
 
 }
